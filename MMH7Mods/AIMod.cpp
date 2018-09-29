66798ec81f7165ed43eab341b3148a2b6dc357eb
LukaOo <dvsoloviev@gmail.com>
2018-09-30 00:28:21 +0300

    Add AI vs AI combats

--- MMH7Mods/AIMod.cpp
+++ MMH7Mods/AIMod.cpp
@@ -17,7 +17,16 @@ AIModConfig::AIModConfig(const ModsConfig& config) :
 
 AIMod::AIMod(const ModsConfig& config) : HookBase("AIMod"),
              _config( config ),
-	        _fAiAdventureMap_Think(this)
+			_pAdventureGridManager(NULL),
+	        _pAdventureController(NULL),
+			_pAttackingHero(NULL),
+			_pDefendingHero(NULL),
+	        _QuickCombatAllowed(1),
+	        _fAiAdventureMap_Think(this),
+			_fH7AdventureGridManager_PostBeginPlay(this),
+	        _fGetInstance(this),
+			_fH7InstantCommandDoCombat_Init(this),
+	        _fH7InstantCommandDoCombat_Execute(this)
 {
 
 }
@@ -26,30 +35,110 @@ AIMod::~AIMod()
 {
 }
 
+int AIMod::H7AdventureGridManager_PostBeginPlay(__int64 This, __int64 Stack_frame, void* pResult)
+{
+	_pAdventureGridManager = (AH7AdventureGridManager*)This;
+	FFrame* pStack = (FFrame*)Stack_frame;
+
+	int retval = ((ProcessInternalPtr)OriginalProcessInternal->get())(This, Stack_frame, pResult);
+
+	return retval;
+}
+
+int AIMod::GetInstanceFun(__int64 This, __int64 Stack_frame, void* pResult)
+{
+	FFrame* pStack = (FFrame*)Stack_frame;
+
+	int retval = ((ProcessInternalPtr)OriginalProcessInternal->get())(This, Stack_frame, pResult);
+
+	AH7AdventureController_execGetInstance_Parms* params = (AH7AdventureController_execGetInstance_Parms*)pResult;
+	_pAdventureController = params->ReturnValue;
+	if (_pAdventureController != NULL && _QuickCombatAllowed)
+	{  
+		// try disable quick combat
+		_QuickCombatAllowed = _pAdventureController->GetAIAllowQuickCombat();
+		LOG(LL_DEBUG) << "Try to disable ai quick comba, before" << _QuickCombatAllowed << "\n";
+		_pAdventureController->SetAIAllowQuickCombat(0);
+		_QuickCombatAllowed = _pAdventureController->GetAIAllowQuickCombat();
+
+		LOG(LL_DEBUG) << "Try to disable ai quick combat, after" << _QuickCombatAllowed << "\n";
+	}
+
+	return retval;
+}
+
+int AIMod::H7InstantCommandDoCombat_Init(__int64 This, __int64 Stack_frame, void* pResult)
+{
+	UH7InstantCommandDoCombat* instantManager = (UH7InstantCommandDoCombat*)This;
+	FFrame * Stack = (FFrame*)Stack_frame;
+	UH7InstantCommandDoCombat_execInit_Parms* pInit_Params = (UH7InstantCommandDoCombat_execInit_Parms*)Stack->Locals;
+	if ( pInit_Params != NULL )
+	{
+		_pAttackingHero = pInit_Params->attackingHero;
+		_pDefendingHero = pInit_Params->defendingHero;
+
+		if (!_QuickCombatAllowed && pInit_Params->isQuickCombat) {
+
+			LOG(LL_DEBUG) << "H7InstantCommandDoCombat_Init QuickCombat flag is " << pInit_Params->isQuickCombat << "\n";
+			pInit_Params->isQuickCombat = 0;
+		}
+	}
+	int retval = ((ProcessInternalPtr)OriginalProcessInternal->get())(This, Stack_frame, pResult);
+	
+	return retval;
+}
+
+int AIMod::H7InstantCommandDoCombat_Execute(__int64 This, __int64 Stack_frame, void* pResult)
+{
+	UH7InstantCommandDoCombat* instantManager = (UH7InstantCommandDoCombat*)This;
+	FFrame * Stack = (FFrame*)Stack_frame;
+	int retval = 0;
+	if (!_QuickCombatAllowed && _pAttackingHero != NULL && _pDefendingHero != NULL) {
+
+		LOG(LL_DEBUG) << "H7InstantCommandDoCombat_Execute QuickCombat flag is " << _QuickCombatAllowed << "\n";
+		_pAttackingHero->GetAdventureArmy()->CleanAllCombatMergePools();
+		_pDefendingHero->GetAdventureArmy()->CleanAllCombatMergePools();
+		if (_pDefendingHero->WorldInfo != NULL) {
+			AH7PlayerReplicationInfo*  replicationInfo = ( AH7PlayerReplicationInfo*) _pDefendingHero->WorldInfo->GetALocalPlayerController()->PlayerReplicationInfo;
+			if (replicationInfo != NULL)
+			{
+				// set combat player as spectator
+				replicationInfo->SetCombatPlayerType(2);
+			}
+		}
+		instantManager->StartCombatMap();
+	}
+	else {
+	    retval = ((ProcessInternalPtr)OriginalProcessInternal->get())(This, Stack_frame, pResult);
+	}
+
+	return retval;
+}
+
 int  AIMod::AiAdventureMap_ThinkFunc(__int64 This, __int64 Stack_frame, void* pResult)
 {
 	AH7AiAdventureMap* aiAdvMap = (AH7AiAdventureMap*)This;
 	FFrame * Stack = (FFrame*)Stack_frame;
 	AH7AiAdventureMap_execThink_Parms* Think_Parms = (AH7AiAdventureMap_execThink_Parms*)Stack->Locals;
 	
-
-	AH7AdventureController* controller = (AH7AdventureController*) UObject::FindObject<UObject>("Class MMH7Game.H7AdventureController");
-	if (controller == NULL) {
+	if (_pAdventureController == NULL) {
 		LOG(LL_VERBOSE) << " Can not find object: AH7AdventureController " << "\n";		
 	}
 	else {
-		controller = controller->GetInstance();
 		if(aiAdvMap == NULL) LOG(LL_VERBOSE) << " AH7AiAdventureMap pointer is NULL " << "\n";
+		else {
+		if (_pAdventureGridManager == NULL) LOG(LL_VERBOSE) << " AdventureGridManager pointer is NULL " << "\n";
 		else {
 			if (Stack->Object == NULL) LOG(LL_VERBOSE) << "Stack UObject pointer is NULL \n";
 			else {
-				if (Think_Parms == NULL) {
-					LOG(LL_NORMAL) << " Error getting Think parameters (NULL)\n";
-				}
-				else {
-					AIAdventureMap aiThinkLogic(controller, aiAdvMap); 
-					if (aiThinkLogic.Think(Think_Parms->Unit, Think_Parms->DeltaTime))
-						return 0;
+					if (Think_Parms == NULL) {
+						LOG(LL_NORMAL) << " Error getting Think parameters (NULL)\n";
+					}
+					else {
+						AIAdventureMap aiThinkLogic(_pAdventureController, aiAdvMap, _pAdventureGridManager);
+						if (aiThinkLogic.Think(Think_Parms->Unit, Think_Parms->DeltaTime))
+							return 0;
+					}
 				}
 			}
 		}
--- MMH7Mods/AIMod.h
+++ MMH7Mods/AIMod.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "HookBase.h"
 
+class AH7AdventureGridManager;
+class AH7AdventureController;
+class AH7AdventureHero;
+
 ///
 /// AIMod configuration
 ///
@@ -34,10 +38,27 @@ private:
 	///
 	AIModConfig _config;
 
+	///
+	/// Game static pointers
+	///
+	AH7AdventureGridManager* _pAdventureGridManager;
+	AH7AdventureController*  _pAdventureController;
+	AH7AdventureHero* _pAttackingHero;
+	AH7AdventureHero* _pDefendingHero;
+	unsigned long _QuickCombatAllowed;
+
 	/// 
 	/// Functions
 	///
-	int AiAdventureMap_ThinkFunc(__int64 pthis, __int64 stach_frame, void* pResult);
+	int AiAdventureMap_ThinkFunc(__int64 pthis, __int64 stack_frame, void* pResult);
+	int H7AdventureGridManager_PostBeginPlay(__int64 pthis, __int64 stack_frame, void* pResult);
+	int GetInstanceFun(__int64 pthis, __int64 stack_frame, void* pResult);
+	int H7InstantCommandDoCombat_Init(__int64 pthis, __int64 stack_frame, void* pResult);
+	int H7InstantCommandDoCombat_Execute(__int64 pthis, __int64 stack_frame, void* pResult);
+
+	///
+	/// Function to initialize grid manager poiner
+	///
 
 	///
 	/// H7AiAdventureMap.Think function
@@ -51,6 +72,63 @@ private:
 			return ((AIMod*)_pBase)->AiAdventureMap_ThinkFunc(This, Stack_frame, pResult);
 		};
 	} _fAiAdventureMap_Think;
+
+	///
+	/// Need to initialize grid manager
+	///
+	class H7AdventureGridManager_PostBeginPlay : public HookFunction
+	{
+	public:
+		H7AdventureGridManager_PostBeginPlay(AIMod* pBase) : HookFunction(pBase, "Function MMH7Game.H7AdventureGridManager.PostBeginPlay") {}
+		int Func(__int64 This, __int64 Stack_frame, void* pResult)
+		{
+			return ((AIMod*)_pBase)->H7AdventureGridManager_PostBeginPlay(This, Stack_frame, pResult);
+		};
+	} _fH7AdventureGridManager_PostBeginPlay;
+
+	///
+    /// Process internal function
+    ///
+	class GetInstance : public HookFunction
+	{
+	public:
+		GetInstance(AIMod* pBase) : HookFunction(pBase, "Function MMH7Game.H7AdventureController.GetInstance") {}
+		int Func(__int64 This, __int64 Stack_frame, void* pResult)
+		{
+			return ((AIMod*)_pBase)->GetInstanceFun(This, Stack_frame, pResult);
+		};
+
+	} _fGetInstance;
+
+	///
+	/// Process internal function H7InstantCommandDoCombat.Init
+	///  try to change mQuickCombatFlag to true
+	///
+	class H7InstantCommandDoCombat_Init : public HookFunction
+	{
+	public:
+		H7InstantCommandDoCombat_Init(AIMod* pBase) : HookFunction(pBase, "Function MMH7Game.H7InstantCommandDoCombat.Init") {}
+		int Func(__int64 This, __int64 Stack_frame, void* pResult)
+		{
+			return ((AIMod*)_pBase)->H7InstantCommandDoCombat_Init(This, Stack_frame, pResult);
+		};
+
+	} _fH7InstantCommandDoCombat_Init;
+
+	///
+	/// Process internal function H7InstantCommandDoCombat.Execute
+	///  try to execute combat Ai vs Ai neutrals
+	///
+	class H7InstantCommandDoCombat_Execute : public HookFunction
+	{
+	public:
+		H7InstantCommandDoCombat_Execute(AIMod* pBase) : HookFunction(pBase, "Function MMH7Game.H7InstantCommandDoCombat.Execute") {}
+		int Func(__int64 This, __int64 Stack_frame, void* pResult)
+		{
+			return ((AIMod*)_pBase)->H7InstantCommandDoCombat_Execute(This, Stack_frame, pResult);
+		};
+
+	} _fH7InstantCommandDoCombat_Execute;
 };
 
 typedef std::shared_ptr<AIMod> AIModPtr;
